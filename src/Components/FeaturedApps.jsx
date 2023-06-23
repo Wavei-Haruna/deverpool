fc92134dd38801fd590f9531c222e24c7868c1cd
wavei-haruna <wavharun2@gmail.com>
2023-06-23 06:55:13 +0000

    font family of Hero section is modified

--- src/Components/FeaturedApps.jsx
+++ src/Components/FeaturedApps.jsx
@@ -5,7 +5,7 @@ export default function Featured() {
   return (
     <main className="my-12 w-full">
       <div className="flex justify-between items-center w-full px-5">
-        <h2 className="text-[22px] font-bold">Featured</h2>
+        <h2 className=" text-xl  md:text-2xl font-hero font-menu">Featured</h2>
         <div className="basic-inputWrapper flex items-center px-2 py-2 rounded ">
           <input
             type="text"
@@ -17,7 +17,7 @@ export default function Featured() {
       </div>
       <div className="grid w-full min-w-[270px] items-center md:grid-cols-2 gap-10 md:justify-between">
         {apps.slice(0, 4).map((app, ai) => (
-          <AppThumbnail app={app} key={ai} />
+          <AppThumbnail app={app} key={ai}  />
         ))}
       </div>
     </main>
